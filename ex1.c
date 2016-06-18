49f0c81f75b8487f35e70d4eb64661c55e61a0fa
Cameron Cairns <cam.cairns1@gmail.com>
2016-06-18 13:25:44 +0100

    Inital Commit
    Learning C from Zed Shaw's "Learn C the Hard Way"
    
    http://c.learncodethehardway.org/book/

---
+++ ex1.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+// int main (int argc, char *argv[])
+int main (int argc, char *argv[])
+{
+    puts("Hello world.\nHello me.\nHello you.\nHello everyone!!!");
+
+    return 0;
+}
